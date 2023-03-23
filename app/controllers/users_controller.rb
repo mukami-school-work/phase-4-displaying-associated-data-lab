a686ffe32276362065a7a7d9b7ce7f6cbda10864
lizdmukami <lizdmukami@gmail.com>
2023-03-23 14:09:20 +0300

    Completed Assignment

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,15 @@
 class UsersController < ApplicationController
+    def show
+        users = User.find(params[:id])
+        render json: users, include: :items
+    end
 end
+
+# class UsersController < ApplicationController
+#   def show
+#     user = User.find(params[:id])
+#     items = user.items
+#     render json: { user: user, items: items }
+#   end
+# end
+
